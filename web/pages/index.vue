ab3900a4e89dba25a3897d3863d7499ad6b91fdd
Panachai <panachai.ny@gmail.com>
2022-05-29 21:41:01 +0700

    make covid page

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,83 +1,127 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
+  <v-card class="mx-auto" max-width="600">
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+          <v-card :color="card.bgColor" height="150px">
+            <v-card-title class="text-h5" :style="{ color: card.titleColor }">{{
+              card.title
+            }}</v-card-title>
+            <v-card-subtitle
+              :style="{
+                color: card.subtitleColor,
+                'font-size': card.subtitleSize + 'px',
+              }"
+              >{{ card.subtitle }}</v-card-subtitle
             >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+            <v-card-text
+              class="text-h2"
+              :style="{
+                color: card.valueColor,
+              }"
+              >{{ card.value }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+const buddhistEra = require('dayjs/plugin/buddhistEra')
+dayjs.extend(buddhistEra)
+
 export default {
   name: 'IndexPage',
+  data: () => ({
+    cards: [],
+  }),
+  async created() {
+    await this.getToday()
+  },
+  methods: {
+    async getToday() {
+      const res = await this.$axios.get('/today', {})
+      const { data } = res.data
+      console.log('data', data)
+
+      // new_case: 3649
+      // new_case_excludeabroad: 3647
+      // new_death: 24
+      // new_recovered: 5622
+      // total_case: 4442648
+      // total_case_excludeabroad: 4417684
+      // total_death: 29972
+      // total_recovered: 4367939
+      // txn_date: "2022-05-29"
+      // update_date: "2022-05-29 07:24:03"
+
+      this.cards = [
+        {
+          title: 'Today',
+          subtitle: dayjs(data.update_date).format('DD MMMM BBBB HH:mm:ss'),
+          subtitleSize: 22,
+          bgColor: 'white',
+          flex: 12,
+        },
+        {
+          title: 'New case',
+          value: this.formatNumber(data.new_case),
+          titleColor: 'white',
+          valueColor: 'white',
+          bgColor: '#DC5A68',
+          flex: 6,
+        },
+        {
+          title: 'Total case',
+          value: this.formatNumber(data.total_case),
+          titleColor: 'white',
+          valueColor: 'white',
+          bgColor: '#DC5A68',
+          flex: 6,
+        },
+        {
+          title: 'Death',
+          value: this.formatNumber(data.new_death),
+          bgColor: '#FFA500',
+          titleColor: 'white',
+          valueColor: 'white',
+          flex: 6,
+        },
+        {
+          title: 'Total Death',
+          value: this.formatNumber(data.total_death),
+          bgColor: '#FFA500',
+          titleColor: 'white',
+          valueColor: 'white',
+          flex: 6,
+        },
+        {
+          title: 'New recovery',
+
+          value: this.formatNumber(data.new_recovered),
+          titleColor: 'white',
+          valueColor: 'white',
+          bgColor: 'green',
+          flex: 6,
+        },
+        {
+          title: 'Total recovery',
+          value: this.formatNumber(data.total_recovered),
+          titleColor: 'white',
+          valueColor: 'white',
+          bgColor: 'green',
+          flex: 6,
+        },
+      ]
+    },
+    formatNumber(v) {
+      return v.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped></style>
